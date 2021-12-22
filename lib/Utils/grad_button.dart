4826c219bd162f1a95e6c7933b74d60853a5b7ec
Mostafij <mostafijul1000@gmail.com>
2021-12-23 03:19:45 +0600

    Working on Cart

---
+++ lib/Utils/grad_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class GradButton extends StatefulWidget {
+  const GradButton({ Key? key }) : super(key: key);
+
+  @override
+  _GradButtonState createState() => _GradButtonState();
+}
+
+class _GradButtonState extends State<GradButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/Utils/product_container.dart
@@ -0,0 +1,19 @@
+import 'package:ecommerce_app/Modules/productmodules.dart';
+import 'package:flutter/material.dart';
+
+
+class ProductContainer extends StatefulWidget {
+  const ProductContainer({ Key? key, ProductInfo? product}) : super(key: key);
+
+  @override
+  _ProductContainerState createState() => _ProductContainerState();
+}
+
+class _ProductContainerState extends State<ProductContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
